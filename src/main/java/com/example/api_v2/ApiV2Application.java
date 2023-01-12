023ea207a0853d1427475e681ed05520c29ccd0f
Şafak Tamses <safak.tamses@gmail.com>
2023-01-12 11:41:32 +0300

    spring-boot security 5 jwt authentication and authorisation

---
+++ src/main/java/com/example/api_v2/ApiV2Application.java
@@ -0,0 +1,13 @@
+package com.example.api_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_v2/ApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
